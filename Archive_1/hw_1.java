ea4bda90fbbb5d983358ce86283d9b544b831152
Rishabh Mitra <rishabh.mitra@learner.manipal.edu>
2020-04-14 17:37:01 -0400

    Initial Commit

---
+++ Archive_1/hw_1.java
@@ -0,0 +1,5 @@
+import java.io.*;
+ public class hw_1{
+  public static void main(String args[]){
+   System.out.println("Hello world");
+}}
---
+++ Archive_1/hw_2.java
@@ -0,0 +1,11 @@
+public class hw_2
+{
+ public static void main(String[] args)
+ {
+  System.out.print("Hi! ");
+  System.out.print(args[0]);
+  System.out.print(" it's nice to meet you!");
+  System.out.println();
+ }
+}
+
