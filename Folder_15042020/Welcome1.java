d8d3538bc0fdb440d4a814ade1f13ad70594a242
Rishabh Mitra <rishabh.mitra@learner.manipal.edu>
2020-04-16 01:46:44 -0400

    Added Welcome1.java as starter

---
+++ Folder_15042020/Welcome1.java
@@ -0,0 +1,10 @@
+public class Welcome1
+{
+ //main method begins the execution of the java program
+ public static void main ( String[] args )
+ {
+  System.out.println("Welcome to Java Programming !");
+  // end method main
+ }
+}
+//end class Welcome1
