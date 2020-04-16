78bb999611bd0a8c7c8375a336e6c71465796fc8
Rishabh Mitra <rishabh.mitra@learner.manipal.edu>
2020-04-16 05:20:31 -0400

    Added Welcome2 and Welcome3

---
+++ Folder_15042020/Welcome2.java
@@ -0,0 +1,12 @@
+/* Welcome2.java */
+public class Welcome2
+{
+ /* main method begins the execution of the Java Program*/
+ public static void main ( String[] args )
+ {
+  System.out.print("Welcome to ");
+  System.out.println("Java Programming !");
+ }
+ /* end of the method main() */
+}
+/* end of the class Welcome2 */
---
+++ Folder_15042020/Welcome3.java
@@ -0,0 +1,11 @@
+/* Welcome3.java */
+public class Welcome3
+{
+ /* main method begins the execution of the Java Program*/
+ public static void main ( String[] args )
+ {
+  System.out.println("Welcome\nto\nJava\nProgramming !");
+ }
+ /* end of the method main() */
+}
+/* end of the class Welcome3 */
